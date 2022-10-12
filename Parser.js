f23bb33f6e7184bcb50ed18810aa6a9058dc3e48
Your Name <you@example.com>
2022-10-12 10:49:47 -0400

    go over the code

--- Parser.js
+++ Parser.js
@@ -2,7 +2,7 @@ function ParseExpression(program) {
   //call the helper function which cut off the space from the begining
   //of the string
   program = skipeSpace(program);
-  console.log(program);
+  //console.log("skip spacd", program);
 
   //create the binding to match and the expression
   let match, expr;
@@ -10,14 +10,12 @@ function ParseExpression(program) {
   // execute the program using RegExp
   if ((match = /^"([^"]*)"/.exec(program))) {
     expr = { type: "value", value: match[1] };
-    console.log(expr.type);
-    console.log(expr.value);
   } else if ((match = /^\d+\b/.exec(program))) {
-    expr = { type: "value", value: Number(match[1]) };
+    expr = { type: "value", value: Number(match[0]) };
   } else if ((match = /^[^\s(),#"]+/.exec(program))) {
     expr = { type: "word", name: match[0] };
   } else {
-    throw new TypeError(" Unexpected syntax " + program);
+    throw new SyntaxError(" Unexpected syntax " + program);
   }
   return parseAply(expr, program.slice(match[0].length));
 }
@@ -26,8 +24,18 @@ function ParseExpression(program) {
 //of programm string
 function skipeSpace(string) {
   let first = string.search(/\S/);
-  console.log(first);
-  if (first === -1) return "";
+
+  console.log("first " + first); //0
+  if (first == -1) return "";
+  console.log("string.slice(first) ", string.slice(first));
+  //string.slice(first)  +(a, 10)
+  //string.slice(first)  (a, 10)
+  //string.slice(first)  a, 10)
+  //string.slice(first)  , 10)
+  //string.slice(first)  10)
+  //string.slice(first)  )
+  //first -1
+
   return string.slice(first);
 }
 
@@ -36,11 +44,22 @@ function skipeSpace(string) {
 // expression to check if it is an application. If so, it
 //parse the parenthesized list of arguments
 function parseAply(expr, program) {
-  program = skipeSpace(program);
-  if (program[0] != "(") {
-    return { expr: expr, rest: program };
-  }
+  console.log("praserapply expr", expr, "prog ", program);
+  // 1 loop: praserapply: expre {type: 'word', name: '+'} name: "+" type: "word" prog  (a, 10)
+  //2nd loop: praserapply: expre {type: 'word', name: 'a'} prog  , 10)
+  //3rd:praserapply: expre {type: 'value', value: 10} prog  )
+  //4th: praserapply: expre {type: 'apply', operator: {…}, args: Array(2)} prog
 
+  program = skipeSpace(program);
+  console.log("prog", program); //+(a, 10)
+  console.log("prog[0]", program[0]); //+
+  if (program[0] != "(") {
+    console.log("expr: expr, rest: program ", { expr: expr, rest: program });
+    // 1st loop: expr: expr, rest: program  {expr: {…}, rest: ', 10)'}
+    // 2 expr: expr, rest: program  {expr: {…}, rest: ')'}
+    // 3 expr: expr, rest: program  {expr: {…}, rest: ''}
+    return { expr: expr, rest: program }; //
+  }
   program = skipeSpace(program.slice(1));
   expr = { type: "apply", operator: expr, args: [] };
   while (program[0] != ")") {
@@ -50,10 +69,10 @@ function parseAply(expr, program) {
     if (program[0] == ",") {
       program = skipeSpace(program.slice(1));
     } else if (program[0] != ")") {
-      throw new TypeError("Expected ,',' of ')'");
+      throw new SyntaxError("Expected ',' or ')'");
     }
   }
-  return parseAply(program.slice(1));
+  return parseAply(expr, program.slice(1));
 }
 
 //now create a parse function that verifies that
@@ -63,7 +82,9 @@ function parseAply(expr, program) {
 function parse(program) {
   let { expr, rest } = ParseExpression(program);
   if (skipeSpace(rest).length > 0) {
-    throw new TypeError("Unexpected text after program");
+    throw new SyntaxError("Unexpected text after program");
   }
   return expr;
 }
+
+console.log(parse("+(a, 10)"));

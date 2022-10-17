4ba9baba4c345c7e374b544493677b79fd47d6db
Your Name <you@example.com>
2022-10-17 07:56:47 -0400

    fixing

--- Parser.js
+++ Parser.js
@@ -93,15 +93,16 @@ console.log(parse("+(a, 10)"));
 //    args: [{type: "word", name: "a"},
 //           {type: "value", value: 10}]}
 
-//const specialForms = Object.create(null);
+const specialForms = Object.create(null);
 
 //create a  function to run the syntex tree
+
 /**
- * 
- * @param {*} expr 
- * @param {*} scope 
- * @returns 
- 
+ * @param {*} expr
+ * @param {*} scope
+ * @returns
+ */
+
 function evaluate(expr, scope) {
   if (expr.type == "value") {
     return expr.value;
@@ -134,33 +135,32 @@ function evaluate(expr, scope) {
     }
   }
 }
-*/
 
-const specialForms = Object.create(null);
+// const specialForms = Object.create(null);
 
-function evaluate(expr, scope) {
-  if (expr.type == "value") {
-    return expr.value;
-  } else if (expr.type == "word") {
-    if (expr.name in scope) {
-      return scope[expr.name];
-    } else {
-      throw new ReferenceError(`Undefined binding: ${expr.name}`);
-    }
-  } else if (expr.type == "apply") {
-    let { operator, args } = expr;
-    if (operator.type == "word" && operator.name in specialForms) {
-      return specialForms[operator.name](expr.args, scope);
-    } else {
-      let op = evaluate(operator, scope);
-      if (typeof op == "function") {
-        return op(...args.map((arg) => evaluate(arg, scope)));
-      } else {
-        throw new TypeError("Applying a non-function.");
-      }
-    }
-  }
-}
+// function evaluate(expr, scope) {
+//   if (expr.type == "value") {
+//     return expr.value;
+//   } else if (expr.type == "word") {
+//     if (expr.name in scope) {
+//       return scope[expr.name];
+//     } else {
+//       throw new ReferenceError(`Undefined binding: ${expr.name}`);
+//     }
+//   } else if (expr.type == "apply") {
+//     let { operator, args } = expr;
+//     if (operator.type == "word" && operator.name in specialForms) {
+//       return specialForms[operator.name](expr.args, scope);
+//     } else {
+//       let op = evaluate(operator, scope);
+//       if (typeof op == "function") {
+//         return op(...args.map((arg) => evaluate(arg, scope)));
+//       } else {
+//         throw new TypeError("Applying a non-function.");
+//       }
+//     }
+//   }
+// }
 
 //add if function to specialForm
 //it two params
